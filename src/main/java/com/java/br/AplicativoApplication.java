b78d41f52cb74b273e7c6d8f74455a95762c9594
eneylton <eneylton@hotmail.com>
2019-05-16 11:50:40 -0300

    Projeto Criado

---
+++ src/main/java/com/java/br/AplicativoApplication.java
@@ -0,0 +1,13 @@
+package com.java.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativoApplication.class, args);
+	}
+
+}

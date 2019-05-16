a0cba4499a694900b0eb7477e6366d95e4720680
eneylton <eneylton@hotmail.com>
2019-05-16 13:06:39 -0300

    Listar Categoria

--- src/main/java/com/java/br/resources/CategoriaResource.java
+++ src/main/java/com/java/br/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.java.br.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.java.br.Model.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest Funcionando !!!!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informática");
+		Categoria cat2 = new Categoria(2,"Escitório");
+		
+		List<Categoria> listar = new ArrayList<>();
+		
+		listar.add(cat1);
+		listar.add(cat2);
+		
+		return listar;
 	}
 
 }
